74556b20ed5704c152b7595bf53077a45028957d
Jeff Gonis <jeffgonis@fastmail.com>
2022-05-29 00:15:42 -0600

    Connecting via TLS?
    My god have I actually connected to the twitch IRC server using TLS?

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+	config := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	_, err := tls.Dial("tcp", "irc.chat.twitch.tv:6697", config)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
